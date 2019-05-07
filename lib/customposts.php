bb7987eab8cdaa443648c9d41e832d09f6cc8366
Gabor Szabo <szabogabi@gmail.com>
2019-05-07 23:25:48 +0200

    gallery cpt added

--- lib/customposts.php
+++ lib/customposts.php
@@ -6,14 +6,14 @@ function marrakesh_custom_post_type() {
     $labels = array(
         'name'                  => _x( 'References', 'Post Type General Name', 'marrakesh' ),
         'singular_name'         => _x( 'Reference', 'Post Type Singular Name', 'marrakesh' ),
-        'menu_name'             => __( 'Reference', 'marrakesh' ),
+        'menu_name'             => __( 'Works & Galleries', 'marrakesh' ),
         'name_admin_bar'        => __( 'Reference', 'marrakesh' ),
         'archives'              => __( 'Item Archives', 'marrakesh' ),
         'attributes'            => __( 'Item Attributes', 'marrakesh' ),
         'parent_item_colon'     => __( 'Parent Item:', 'marrakesh' ),
         'all_items'             => __( 'All References', 'marrakesh' ),
         'add_new_item'          => __( 'Add New Item', 'marrakesh' ),
-        'add_new'               => __( 'Add New', 'marrakesh' ),
+        'add_new'               => __( 'Add New Reference', 'marrakesh' ),
         'new_item'              => __( 'New Item', 'marrakesh' ),
         'edit_item'             => __( 'Edit Item', 'marrakesh' ),
         'update_item'           => __( 'Update Item', 'marrakesh' ),
@@ -53,6 +53,57 @@ function marrakesh_custom_post_type() {
         'capability_type'       => 'page',
     );
     register_post_type( 'reference', $args );
+
+    // helper galleries
+    $labels = array(
+        'name'                  => _x( 'Galleries', 'Post Type General Name', 'marrakesh' ),
+        'singular_name'         => _x( 'Gallery', 'Post Type Singular Name', 'marrakesh' ),
+        'menu_name'             => __( 'Gallery', 'marrakesh' ),
+        'name_admin_bar'        => __( 'Gallery', 'marrakesh' ),
+        'archives'              => __( 'Item Archives', 'marrakesh' ),
+        'attributes'            => __( 'Item Attributes', 'marrakesh' ),
+        'parent_item_colon'     => __( 'Parent Item:', 'marrakesh' ),
+        'all_items'             => __( 'Galleries', 'marrakesh' ),
+        'add_new_item'          => __( 'Add New Item', 'marrakesh' ),
+        'add_new'               => __( 'Add New Gallery', 'marrakesh' ),
+        'new_item'              => __( 'New Item', 'marrakesh' ),
+        'edit_item'             => __( 'Edit Item', 'marrakesh' ),
+        'update_item'           => __( 'Update Item', 'marrakesh' ),
+        'view_item'             => __( 'View Gallery', 'marrakesh' ),
+        'view_items'            => __( 'View Galleries', 'marrakesh' ),
+        'search_items'          => __( 'Search Gallery', 'marrakesh' ),
+        'not_found'             => __( 'Not found', 'marrakesh' ),
+        'not_found_in_trash'    => __( 'Not found in Trash', 'marrakesh' ),
+        'featured_image'        => __( 'Featured Image', 'marrakesh' ),
+        'set_featured_image'    => __( 'Set featured image', 'marrakesh' ),
+        'remove_featured_image' => __( 'Remove featured image', 'marrakesh' ),
+        'use_featured_image'    => __( 'Use as featured image', 'marrakesh' ),
+        'insert_into_item'      => __( 'Insert into item', 'marrakesh' ),
+        'uploaded_to_this_item' => __( 'Uploaded to this item', 'marrakesh' ),
+        'items_list'            => __( 'Items list', 'marrakesh' ),
+        'items_list_navigation' => __( 'Items list navigation', 'marrakesh' ),
+        'filter_items_list'     => __( 'Filter items list', 'marrakesh' ),
+    );
+    $args = array(
+        'label'                 => __( 'Gallery', 'marrakesh' ),
+        'description'           => __( 'Post Type Description', 'marrakesh' ),
+        'labels'                => $labels,
+        'supports'              => array( 'title', 'editor', 'thumbnail', 'page-attributes', 'custom-fields', 'excerpt' ),
+        'hierarchical'          => false,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_menu'          => 'edit.php?post_type=reference',
+        'menu_position'         => 21,
+        'menu_icon'             => 'dashicons-images-alt2',
+        'show_in_admin_bar'     => true,
+        'show_in_nav_menus'     => true,
+        'can_export'            => true,
+        'has_archive'           => true,
+        'exclude_from_search'   => false,
+        'publicly_queryable'    => true,
+        'capability_type'       => 'page',
+    );
+    register_post_type( 'gallery', $args );
 
 }
 add_action( 'init', 'marrakesh_custom_post_type', 0 );

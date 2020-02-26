8e3e19d6cd24a3c11bbe3f58acfd001099e9ae34
Gabor Szabo <szabogabi@gmail.com>
2020-02-26 22:30:14 +0100

    hotjar tracking added

--- templates/head.php
+++ templates/head.php
@@ -17,6 +17,17 @@ $current_url = home_url( add_query_arg( array(), $wp->request ) );
     <meta property="og:url" content="<?= $current_url; ?>" />
     <?php wp_head(); ?>
     <?php if ( !current_user_can( 'manage_options' ) ) : ?>
+    <!-- Hotjar Tracking Code for https://marrakeshcementlap.hu -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:1706309,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
     <!-- Global site tag (gtag.js) - Google Ads: 734728363 -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=AW-734728363"></script>
     <script>

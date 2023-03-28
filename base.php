75ae34c01281c200d14a01cbaa70cf7c05b2d22d
Gabor Szabo <szabogabi@gmail.com>
2023-03-28 11:47:19 +0200

    GTM container added

--- base.php
+++ base.php
@@ -9,12 +9,13 @@ use Roots\Sage\Wrapper;
 <html <?php language_attributes(); ?>>
   <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
+    <?php if ( !current_user_can( 'manage_options' ) && (ENV=='production')) : ?>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N5DS238"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+    <?php endif; ?>
 
-    <!--[if IE]>
-      <div class="alert alert-warning">
-        <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
-      </div>
-    <![endif]-->
     <?php
         do_action('get_header');
         get_template_part('templates/header');
--- templates/head.php
+++ templates/head.php
@@ -17,27 +17,12 @@ $current_url = home_url( add_query_arg( array(), $wp->request ) );
     <meta property="og:url" content="<?= $current_url; ?>" />
     <?php wp_head(); ?>
     <?php if ( !current_user_can( 'manage_options' ) && (ENV=='production')) : ?>
-    <!-- Hotjar Tracking Code for https://marrakeshcementlap.hu -->
-    <script>
-        (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:1706309,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    </script>
-    <!-- Global site tag (gtag.js) - Google Ads: 734728363 -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-734728363"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-        gtag('config', 'AW-734728363');
-        gtag('config', 'UA-3523093-7');
-    </script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-N5DS238');</script>
+    <!-- End Google Tag Manager -->
     <?php endif; ?>
 </head>
